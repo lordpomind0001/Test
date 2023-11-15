0d53f4b5e48370bedcac2fb37ea384b0136f98e9
lordpomind0001 <118119431+lordpomind0001@users.noreply.github.com>
2023-11-15 18:40:24 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+app = Flask(__name__)
+
+# Load pre-trained model and tokenizer
+model = GPT2LMHeadModel.from_pretrained("EleutherAI/gpt-neo-1.3B")
+tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.form['user_input']
+    conversation_history = f"You: {user_input}\nBot:"
+    
+    # Tokenize the conversation history
+    input_ids = tokenizer.encode(conversation_history, return_tensors="pt")
+    
+    # Generate a response using the model
+    output = model.generate(input_ids, max_length=150, num_beams=5, no_repeat_ngram_size=2, top_k=50, top_p=0.95, temperature=0.7)
+    
+    # Decode the generated output
+    generated_response = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    return render_template('index.html', user_input=user_input, bot_response=generated_response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
